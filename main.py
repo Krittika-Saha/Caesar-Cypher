f07532c2f13a14df99976a7e7e80142695945915
Krittika-Saha <krittika.saha.dev@gmail.com>
2020-11-29 19:21:44 +0530

    Challenge 4 completed(Project Finished)
    I looked at the solution halfway through 😛.

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-#variables, lists and inputs
+#variables, lists, imports and inputs
+from art import logo
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
 # Encrypt Function
-def encrypt(text, shift):
+def caesar(text, shift, direction):
   encoded_list = []
   for i in text:
-    encoded_list.append(alphabet[alphabet.index(i)+shift])
+    if i in alphabet:
+      encoded_list.append(alphabet[alphabet.index(i)+shift])
+    else:
+      encoded_list.append(i)
   encoded_text = ''.join(encoded_list)
-  print(f"Result: {encoded_text}")
+  print(f"{direction}d Text: {encoded_text}")
 
 
 # Code which helps in running
+print(logo)
 while True:
   direction = input("Type 'encode' to encrypt, type 'decode' to decrypt, type 'exit' to exit:\n")
   if direction == 'exit':
@@ -21,9 +26,15 @@ while True:
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number(integer):\n"))
     if direction == 'encode':
-      encrypt(text, shift)
+      if shift >= 25:
+        caesar(text, shift%25, 'encode')
+      else:
+        caesar(text, shift, 'encode')
     elif direction == 'decode':
-      encrypt(text, -shift)
+      if shift >= 25:
+        caesar(text, -shift%25, 'decode')
+      else:
+        caesar(text, -shift, 'decode')
     else:
       print("Sorry, I don't understand.")
   else:
